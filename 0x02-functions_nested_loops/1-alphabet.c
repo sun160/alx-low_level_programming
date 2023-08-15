40e5eb904758d7b08eaac8957107fc15cfdf7bf1
sun160 <Nour>
2023-08-15 07:23:45 -0700

    nnj

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,9 @@
 #include "main.h"
 
 /**
- * main - Entry point
  *
  * Description: print the alphabet, in lowercase, followed by a new line
  *
- * Return: 0 (success)
 */
 
 int main(void)
@@ -15,6 +13,4 @@ int main(void)
 	for (ch = 'a'; ch <= 'z'; ch++)
 		_putchar(ch);
 	_putchar('\n');
-
-	return (0);
 }
