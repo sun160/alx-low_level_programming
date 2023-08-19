72a0c6624c5387b2386304b3ce8a80aee740e468
sun160 <Nour>
2023-08-19 02:40:18 -0700

    ff1

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -11,8 +11,8 @@ void print_numbers(void)
 	int num = 0;
 
 	do {
-		_putchar(num + 48)
+		_putchar(num + 48);
 		num++;
-	} while (num >= 0 && num <= 9)
+	} while (num >= 0 && num <= 9);
 	_putchar('\n');
 }
