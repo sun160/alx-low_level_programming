3c604c711c4e7fb302f09e331bff4ccf75c5d07a
sun160 <Nour>
2023-08-19 02:33:58 -0700

    ff

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		if (num == 2 || num == 4)
+			continue;
+		_putchar(num + 48);
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,6 +7,9 @@
 int _putchar(char);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
 
 
 
