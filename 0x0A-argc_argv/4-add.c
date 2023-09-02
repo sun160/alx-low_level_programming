0e4fcbc5231c32a169d32e6ef1c8d01a5708f0f1
sun160 <Nour>
2023-09-01 20:13:52 -0700

    rrf11s

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
 	while (--argc)
 	{
 		for (i = argv[argc]; *i; i++)
-			if (*i < 0 || *i > 9)
+			if (*i < '0' || *i > '9')
 				return (printf("Error\n"), 1);
 		sum += atoi(argv[argc]);
 	}
