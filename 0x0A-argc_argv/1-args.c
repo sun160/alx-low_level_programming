ecded1d0477fd90ccccc302880c4308904a7471e
sun160 <Nour>
2023-09-02 02:12:09 -0700

    dd1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,11 +4,12 @@
  * main - prints the number of arguments passed into it
  *
  * @argc: The number of command line arguments
+ * @argv: array containing the program command line arguments
  *
  * Return: 0 if success
 */
 
-int main(int argc)
+int main(int argc, char *argv[])
 {
 	printf("%d\n", argc - 1);
 	return (0);
