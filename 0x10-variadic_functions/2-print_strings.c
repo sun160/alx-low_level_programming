99b19b746d871289894974ce97483ce46bce9f84
sun160 <Nour>
2023-09-14 19:45:39 -0700

    ff

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,7 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		return;
 	}
 	while (t--)
-		printf("%s%s", (str = va_arg(ptr, char *)) ? str : "nil",
+		printf("%s%s", (str = va_arg(ptr, char *)) ? str : "(nil)",
 				t ? (separator ? separator : "") : "\n");
 	va_end(ptr);
 }
