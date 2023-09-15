4b33cc26b661bd1e84e04a733cd168f08eb3ade4
sun160 <Nour>
2023-09-15 16:39:20 -0700

    ffd

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -22,5 +22,10 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void format_char(char *separator, va_list ptr);
+void format_int(char *separator, va_list ptr);
+void format_float(char *separator, va_list ptr);
+void format_string(char *separator, va_list ptr);
+
 
 #endif
