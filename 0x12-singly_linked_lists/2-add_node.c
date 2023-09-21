1953a4b031a2b65446bcced8c7d5d5803d09bcdf
sun160 <n.abdelmonem@yahoo.com>
2023-09-21 16:29:37 -0700

    linked list4

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,19 +10,14 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_begin = malloc(sizeof(list_t));
+	int l = 0;
 
 	if (!head || !new_begin)
 		return (NULL);
-	if (str)
-	{
-		new_begin->str = strdup(str);
-		if (!new_begin->str)
-		{
-			free(new_begin);
-			return (NULL);
-		}
-		new_begin->len = _strlen(new_begin->str);
-	}
+	while (str[l])
+		l++;
+	new_begin->len = l;
+	new_begin->str = strdup(str);
 	new_begin->next = *head;
 	*head = new_begin;
 	return (new_begin);
