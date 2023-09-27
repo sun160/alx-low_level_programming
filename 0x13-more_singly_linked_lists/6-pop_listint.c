0d1bb2d1a9e59c8fe5ae944347478ac15e514130
sun160 <n.abdelmonem@yahoo.com>
2023-09-26 17:02:21 -0700

    linked lists

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * pop_listint - deletes the head node 
+ * pop_listint - deletes the head node
  * @head: pointer to the first node
  *
  * Return: the data inside the deleted node
