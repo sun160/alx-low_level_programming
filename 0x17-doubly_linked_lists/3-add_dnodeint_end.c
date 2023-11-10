410ae088da33b74b603d1408420bf72644a0f106
sun160 <you@example.com>
2023-11-10 11:36:26 -0800

    new node

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,37 +1,37 @@
 #include "lists.h"
 
 /**
- * add_dnodeint - adds a new head node to list
+ * add_dnodeint_end - adds a new node at the end of list
  *
  * @head: head of the list
  * @n: value of the element
  * Return: the address of the new element
-*/
-
-dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *new;
 	dlistint_t *d;
+	dlistint_t *new;
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
 
 	new->n = n;
-	new->prev = NULL;
+	new->next = NULL;
 	d = *head;
 
 	if (d != NULL)
 	{
-		while (d->prev != NULL)
-			d = d->prev;
+		while (d->next != NULL)
+			d = d->next;
+		d->next = new;
+	}
+	else
+	{
+		*head = new;
 	}
 
-	new->next = d;
-
-	if (d != NULL)
-		d->prev = new;
-	*head = new;
+	new->prev = d;
 
 	return (new);
 }
