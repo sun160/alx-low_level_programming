0b6f4613052d58423bbbed3880667021cfb1c42d
sun160 <you@example.com>
2023-11-10 19:02:33 -0800

    node6

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,1 +1,27 @@
 #include "lists.h"
+
+/**
+ * get_dnodeint_at_index - returns the node of list
+ * @head: pointer to head of the list
+ * @index: index of the node to return
+ * Return: address of the node
+*/
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int m;
+
+	m = 0;
+
+	if (head == NULL)
+		return (NULL);
+
+	while (head)
+	{
+		if (index == m)
+			return (head);
+		m++;
+		head = head->next;
+	}
+	return (NULL);
+}
